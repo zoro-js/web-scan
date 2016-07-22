22140cd28151bfdfc9dc71da0337f5086f6e4e07
Yinyga Zhang <zyy7259@gmail.com>
2016-07-22 15:12:35 +0800

    add config.context

--- index.js
+++ index.js
@@ -3,20 +3,58 @@
 * @Date:   2016-07-22T13:50:42+08:00
 * @Email:  zyy7259@gmail.com
 * @Last modified by:   zyy
-* @Last modified time: 2016-07-22T14:00:49+08:00
+* @Last modified time: 2016-07-22T15:12:08+08:00
 */
 
 var argv = require('yargs')
+  .usage('Usage: $0 [options]')
   .config()
+  .option('pattern', {
+    describe: 'minimatch pattern',
+    required: true
+  })
+  .option('context', {
+    describe: 'The base directory for resolving the `pattern` (absolute or relative; if relative, relative to process.cwd())'
+  })
   .check(function (parsedArgv, aliasMap) {
     if (!parsedArgv.pattern) {
       throw 'Missing required config "pattern"'
     }
     return true
   })
+  .example('$0 --config config.json', 'read JSON config file')
+  .example('$0 --pattern *', 'command line arguments')
   .argv
 
+console.log(argv)
+
+var pattern = argv.pattern
+if (!Array.isArray(pattern)) {
+  pattern = [pattern]
+}
+
+var path = require('path')
+var context = argv.context
+if (context) {
+  context = path.resolve(process.cwd(), context)
+  pattern = pattern.map(function (item) {
+    if (item === '*') {
+      return path.join(context, '/**/*')
+    } else {
+      var index = 0
+      var prefix = ''
+      if (item.indexOf('!') === 0) {
+        index = 1
+        prefix = '!'
+      }
+      return prefix + path.join(context, item.slice(index))
+    }
+  })
+}
+
+console.log(pattern)
+
 var globby = require('globby')
-globby(argv.pattern).then(function (paths) {
+globby(pattern).then(function (paths) {
   console.log(paths)
 })
